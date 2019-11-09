a17007847afeaf84217c14fd170157e16d819f0b
AndreaCecilia <ac.moreno432@uniandes.edu.co>
2019-11-09 17:17:02 -0500

    Se agrega el Ejercicio 4

---
+++ Ejercicio4.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int getMaxInt(int a,int b)
+{
+  if(a>b){
+   return (a);}
+   else{return b;}
+}
+double getMaxDouble(double a, double b)
+{
+      if(a>b){
+   return (a);}
+   else{return b;}
+}
+
+int main(void)
+{
+    int x=5,y=2;
+    double n=5.0,m=2.0;
+    std::cout<< getMaxInt(x,y)<<"\n";
+    std::cout<< getMaxDouble(n,m)<<"\n";
+    return 0;
+}
+
+
+
