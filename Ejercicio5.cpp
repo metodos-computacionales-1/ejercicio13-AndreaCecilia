14b0598e37ec446333b4354dc34398622f8a65b9
AndreaCecilia <ac.moreno432@uniandes.edu.co>
2019-11-12 14:25:13 -0500

    Se añade

---
+++ Ejercicio5.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <cmath>
+const int VAR=4;
+int main(void)
+{
+    std::cout<<std::sin(VAR)<<std::endl;
+    std::cout<<M_PI<<std::endl;
+    std::cout<<std::cos(M_PI)<<std::endl;
+    return 0;
+}
